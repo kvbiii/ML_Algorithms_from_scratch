03b7e6b992b2293cf601ae51d4852a1f8505721f
Kuba <kuba.koscian001@gmail.com>
2023-07-07 17:54:15 +0200

    Requirements updated

--- requirements.py
+++ requirements.py
@@ -6,8 +6,11 @@ import plotly
 from plotly.subplots import make_subplots
 import plotly.graph_objs as go
 import plotly.figure_factory as ff
+from plotly import express as px
 import plotly.io as pio
 import kaleido
 import torch
 from scipy import stats
-from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+from statsmodels.graphics.gofplots import qqplot
+from matplotlib import pyplot as plt
