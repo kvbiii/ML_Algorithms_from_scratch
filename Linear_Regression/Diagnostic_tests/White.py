de030254c8e41f36f99b22946c0ddbb24f18f2ee
Kuba <kuba.koscian001@gmail.com>
2023-07-10 14:07:56 +0200

    White test

--- Linear_Regression/Diagnostic_tests/White.py
+++ Linear_Regression/Diagnostic_tests/White.py
@@ -3,33 +3,56 @@ import sys
 path_root = Path(__file__).parents[2]
 sys.path.append(str(path_root))
 from requirements import *
-path_root = Path(__file__).parents[1]
-sys.path.append(str(path_root))
-from Metrics import *
+from Metrics.Regression_metrics import *
 
 class White():
     def __init__(self):
         pass
     
-    def test(self, fitted_model, interaction_columns_used_in_model):
-        if not isinstance(interaction_columns_used_in_model, np.ndarray) and not isinstance(interaction_columns_used_in_model, list):
-            raise TypeError("Wrong type, it should be list or numpy array.")
-        self.interaction_columns_used_in_model = interaction_columns_used_in_model
+    def test(self, fitted_model):
+        model, X, features_names, residuals_squared = self.read_fitted_model_attributes(fitted_model=fitted_model)
+        X, features_names = self.remove_intercept(model=model, X=X, features_names=features_names)
+        interactions_indices = self.find_interaction_columns(X=X, features_names=features_names)
+        X, features_names = self.remove_interaction_columns(X=X, features_names=features_names, interactions_indices=interactions_indices)
+        new_model = self.fit_model_with_residuals(model=model, X=X, residuals=residuals_squared, features_names=features_names, target_name="residuals_squared")
+        self.F_test, self.p_value = self.calculate_F_test_and_p_value(model=new_model, X=new_model.X, y=new_model.y, features_names=new_model.features_names)
+
+    def read_fitted_model_attributes(self, fitted_model):
+        mod_class = fitted_model.__class__
+        model = mod_class(fit_intercept=fitted_model.fit_intercept, optimization=True, degree=fitted_model.degree)
         X = fitted_model.X.copy()
         features_names = fitted_model.features_names.copy()
-        y_pred_restricted = fitted_model.predict(X=X, features_names=features_names).copy()
-        residuals_squared = (fitted_model.y-y_pred_restricted)**2
-        mod_class = fitted_model.__class__
-        if(fitted_model.fit_intercept == True):
+        y_pred = fitted_model.predict(X=X, features_names=features_names).copy()
+        residuals_squared = (fitted_model.y-y_pred)**2
+        return model, X, features_names, residuals_squared
+    
+    def remove_intercept(self, model, X, features_names):
+        if(model.fit_intercept == True):
             X = np.delete(X, 0, 1)
             features_names.remove("Intercept")
-        i = 0
-        while(i < len(self.interaction_columns_used_in_model)):
-            index = features_names.index(self.interaction_columns_used_in_model[i])
-            features_names.remove(self.interaction_columns_used_in_model[i])
-            X = np.delete(X, index, axis=1)
-            i = i + 1
-        new_model = mod_class(fit_intercept=fitted_model.fit_intercept, optimization=True, degree=2)
-        new_model.fit(X=X, y=residuals_squared, features_names=features_names, target_name="residuals_squared", diagnostic_test=True)
-        self.F_test = new_model.calculate_F_test(y_true=new_model.y, y_pred=new_model.predict(X=new_model.X, features_names=new_model.features_names), number_of_independent_variables_and_const=len(new_model.features_names))
-        self.p_value = np.round(new_model.calculate_p_value_F_test(F_test=self.F_test, dfn=len(features_names), dfd=len(X)-len(features_names)), 5)
+        return X, features_names
+    
+    def find_interaction_columns(self, X, features_names):
+        features_names = np.array(features_names)
+        interactions_and_polynomial_indices = np.union1d(np.where(np.char.find(features_names, "*")!=-1), np.where(np.char.find(features_names, "^")!=-1))
+        return np.array(interactions_and_polynomial_indices)
+
+    def remove_interaction_columns(self, X, features_names, interactions_indices):
+        if(len(interactions_indices) == 0):
+            pass
+        else:
+            features_names = np.delete(features_names, interactions_indices, axis=0).tolist()
+            X = np.delete(X, interactions_indices, axis=1)
+        return X, features_names
+    
+    def fit_model_with_residuals(self, model, X, residuals, features_names, target_name):
+        model.fit(X=X, y=residuals, features_names=features_names, target_name=target_name, diagnostic_test=True)
+        return model
+        
+    def calculate_F_test_and_p_value(self, model, X, y, features_names):
+        R_squared = R_square(y_true=y, y_pred=model.predict(X=X, features_names=features_names))
+        nominator = (R_squared/len(features_names))
+        denominaotr = (1-R_squared)/(len(X)-len(features_names)-1)
+        F_test = np.round(nominator/denominaotr, 5)
+        p_value = np.round(model.calculate_p_value_F_test(F_test=F_test, dfn=len(features_names), dfd=len(X)-len(features_names)-1), 5)
+        return F_test, p_value
