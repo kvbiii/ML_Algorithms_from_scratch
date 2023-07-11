a0d6e85ba5707099315582018b1a616a0e589f0e
Kuba <kuba.koscian001@gmail.com>
2023-07-11 18:49:25 +0200

    Unusual and outliers observations

--- Linear_Regression/Problems_with_data/Unusual_Outlier_Observations.py
+++ Linear_Regression/Problems_with_data/Unusual_Outlier_Observations.py
@@ -3,12 +3,9 @@ import sys
 path_root = Path(__file__).parents[2]
 sys.path.append(str(path_root))
 from requirements import *
-path_root = Path(__file__).parents[1]
-sys.path.append(str(path_root))
-from Metrics import *
 
 class Unusual_outlier_observations():
-    def __init__(self,):
+    def __init__(self):
         pass
 
     def test(self, fitted_model):
@@ -17,6 +14,7 @@ class Unusual_outlier_observations():
         self.standarized_residuals = self.calculate_standarized_residuals(X=X, residuals=residuals, leverage=self.leverage)
         self.cook_distance = self.calculate_cook_distance(X=X, standarized_residuals=self.standarized_residuals, leverage=self.leverage)
         self.summary_dataframe = self.summary(leverage=self.leverage, standarized_residuals=self.standarized_residuals, cook_distance=self.cook_distance)
+        self.indices_of_outliers = self.find_indices_of_outliers(X=X, standarized_residuals=self.standarized_residuals, leverage=self.leverage, cook_distance=self.cook_distance, summary_dataframe=self.summary_dataframe)
     
     def read_fitted_model_attributes(self, fitted_model):
         X = fitted_model.X.copy()
@@ -38,3 +36,6 @@ class Unusual_outlier_observations():
     
     def summary(self, leverage, standarized_residuals, cook_distance):
         return pd.DataFrame({'Leverage': leverage, 'Standarized_residuals': standarized_residuals, "Cook_Distance": cook_distance})
+    
+    def find_indices_of_outliers(self, X, standarized_residuals, leverage, cook_distance, summary_dataframe):
+        return summary_dataframe[(leverage>2*X.shape[1]/len(X)) & (np.abs(standarized_residuals) > 2) & (cook_distance > 4/X.shape[0])].index.tolist()
