d45f9efc640830bf8e306cb412632345cc470a08
Kuba <kuba.koscian001@gmail.com>
2023-08-04 20:56:12 +0200

    Requirements updated

--- requirements.py
+++ requirements.py
@@ -10,7 +10,7 @@ from plotly import express as px
 import plotly.io as pio
 import kaleido
 import torch
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, OrdinalEncoder
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, OrdinalEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split, KFold, StratifiedKFold
 from statsmodels.graphics.gofplots import qqplot
 from matplotlib import pyplot as plt
@@ -19,3 +19,5 @@ from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
 import itertools
 from tabulate import tabulate
 import kds
+from collections import Counter
+from arch.bootstrap import MovingBlockBootstrap
