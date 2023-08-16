d1be5016520970c64837ca204aa74c0bbc353a81
Kuba <kuba.koscian001@gmail.com>
2023-08-16 13:48:54 +0200

    Lasso Regularization from scratch

--- Regularization/Model/Lasso_from_scratch.py
+++ Regularization/Model/Lasso_from_scratch.py
@@ -5,7 +5,7 @@ sys.path.append(str(path_root))
 from requirements import *
 
 class Lasso_Regressor():
-    def __init__(self, alpha=1, fit_intercept=True, learning_rate=0.01, max_iter=500, random_state=17):
+    def __init__(self, alpha=1, fit_intercept=True, learning_rate=0.1, max_iter=500, random_state=17):
         self.alpha = alpha
         self.fit_intercept = fit_intercept
         self.learning_rate = learning_rate
@@ -56,33 +56,27 @@ class Lasso_Regressor():
         return X
 
     def initialize_weights(self):
-        init_coef = np.random.random(size=(self.X_train.shape[1], 1))
-        return init_coef
+        return np.random.random(size=(self.X_train.shape[1],))
     
     def find_coefficients_gradient_descent(self, X, y, learning_rate, max_iter):
         self.losses = np.zeros(shape=(max_iter, ))
         for epoch in range(0, max_iter):
-            y_predict = self.linear_model(X=X, coef=self.coef_)
-            grad_coef = self.calculate_derivative_of_loss(X=X, y=y, y_predict=y_predict, alpha=self.alpha)
+            self.losses[epoch] = self.calculate_loss(X=X, y=y, coefs=self.coef_, alpha=self.alpha)
+            grad_coef = self.calculate_derivative_of_loss(X=X, y=y, coefs=self.coef_, alpha=self.alpha)
             self.coef_ = self.coef_ - learning_rate*grad_coef
-            self.losses[epoch] = self.calculate_loss(X=X, y=y, y_predict=y_predict)
     
-    def linear_model(self, X, coef):
-        return np.matmul(X, coef).squeeze()
-    
-    def calculate_derivative_of_loss(self, X, y, y_predict, alpha):
-        return (-2/X.shape[0]*(np.matmul(X.T, y-y_predict)-alpha/2*np.sum(self.coef_/np.abs(self.coef_)))).reshape(-1,1)
+    def calculate_loss(self, X, y, coefs, alpha):
+        return 1/X.shape[0]*(np.sum(y-np.matmul(coefs, X.T))**2+alpha*np.sum(np.abs(coefs)))
 
-    def calculate_loss(self, X, y, y_predict):
-        return 1/X.shape[0]*(np.sum(y-y_predict)**2+self.alpha*np.sum(np.abs(self.coef_)))
+    def calculate_derivative_of_loss(self, X, y, coefs, alpha):
+        return -2/X.shape[0]*(np.dot(X.T, y-np.matmul(coefs, X.T))-alpha/2*coefs/np.abs(coefs))
     
     def predict(self, X):
         self.check_fit(fit_used=self.fit_used)
         X = self.check_X(X=X, train=False)
         X = self.check_for_object_columns(X=X)
         X = self.check_intercept(X=X)
-        self.original_shape[0] = X.shape[0]
-        y_predict = np.matmul(X, self.coef_).reshape(self.original_shape)
+        y_predict = np.matmul(X, self.coef_)
         return y_predict
     
     def check_fit(self, fit_used):
@@ -91,7 +85,7 @@ class Lasso_Regressor():
 
 
 class Lasso_Classifier():
-    def __init__(self, alpha=1, fit_intercept=True, learning_rate=0.01, max_iter=500, random_state=17):
+    def __init__(self, alpha=1, fit_intercept=True, learning_rate=0.1, max_iter=500, random_state=17):
         self.alpha = alpha
         self.fit_intercept = fit_intercept
         self.learning_rate = learning_rate
@@ -103,6 +97,7 @@ class Lasso_Classifier():
     def fit(self, X, y):
         self.X_train = self.check_X(X=X, train=True)
         self.y_train = self.check_y(y=y)
+        self.y_train = self.change_to_one_hot_encode(y=self.y_train)
         self.X_train = self.check_for_object_columns(X=self.X_train)
         self.X_train = self.check_intercept(X=self.X_train)
         self.coef_ = self.initialize_weights()
@@ -126,14 +121,15 @@ class Lasso_Classifier():
         y = np.array(y)
         if(y.ndim == 2):
             y = y.squeeze()
+        self.original_classes = np.unique(y)
+        self.number_of_classes = len(self.original_classes)
+        return y
+    
+    def change_to_one_hot_encode(self, y):
         encoder = OneHotEncoder(sparse_output=False)
-        y_encoded = encoder.fit_transform(y.reshape(-1,1))
-        #Transform to have only 1 and -1 classes
-        y_encoded = np.where(y_encoded == 1, 1, -1)
-        self.original_categories = encoder.categories_[0]
-        if(y_encoded.shape[1] == 2):
-            y_encoded = y_encoded[:,1]
-        return y_encoded
+        y_transformed = encoder.fit_transform(y.reshape(-1,1))
+        y_transformed = np.where(y_transformed == 1, 1, -1)
+        return y_transformed
     
     def check_for_object_columns(self, X):
         if not isinstance(X, pd.DataFrame):
@@ -148,78 +144,56 @@ class Lasso_Classifier():
         return X
     
     def initialize_weights(self):
-        if(len(self.original_categories) == 2):
-            init_coef = np.random.random(size=(self.X_train.shape[1], 1))
-        else:
-            init_coef = np.random.random(size=(self.X_train.shape[1], self.y_train.shape[1]))
-        return init_coef
+        return np.random.random(size=(self.X_train.shape[1], self.number_of_classes))
     
     def find_coefficients_gradient_descent(self, X, y, learning_rate, max_iter):
-        if(len(self.original_categories)==2):
-            self.losses = np.zeros(shape=(max_iter, ))
+        self.losses = np.zeros(shape=(max_iter, self.number_of_classes))
+        for klasa in range(0, self.number_of_classes):
+            current_coef = self.coef_[:, klasa]
             for epoch in range(0, max_iter):
-                    y_predict = self.linear_model(X=X, coef=self.coef_)
-                    grad_coef = self.calculate_derivative_of_loss(X=X, y=y, y_predict=y_predict, coef=self.coef_, alpha=self.alpha)
-                    self.coef_ = self.coef_ - learning_rate*grad_coef
-                    self.losses[epoch] = self.calculate_loss(X=X, y=y, y_predict=y_predict, coef=self.coef_)
-        else:
-            self.losses = np.zeros(shape=(max_iter, len(self.original_categories)))
-            for klasa in range(0, len(self.original_categories)):
-                for epoch in range(0, max_iter):
-                    y_predict = self.linear_model(X=X, coef=self.coef_[:,klasa])
-                    grad_coef = self.calculate_derivative_of_loss(X=X, y=y[:,klasa], y_predict=y_predict, coef=self.coef_[:,klasa], alpha=self.alpha)
-                    self.coef_[:,klasa] = self.coef_[:,klasa] - learning_rate*grad_coef.squeeze()
-                    self.losses[epoch, klasa] = self.calculate_loss(X=X, y=y[:,klasa], y_predict=y_predict, coef=self.coef_[:,klasa])
-            self.losses = np.mean(self.losses, axis=1)
+                self.losses[epoch, klasa] = self.calculate_loss(X=X, y=y[:, klasa], coefs=current_coef, alpha=self.alpha)
+                grad_coef = self.calculate_derivative_of_loss(X=X, y=y[:,klasa], coefs=current_coef, alpha=self.alpha)
+                current_coef = current_coef - learning_rate*grad_coef
+            self.coef_[:, klasa] = current_coef
     
-    def linear_model(self, X, coef):
-        return np.matmul(X, coef).squeeze()
-    
-    def calculate_derivative_of_loss(self, X, y, coef, y_predict, alpha):
-        return (-2/X.shape[0]*(np.matmul(X.T, y-y_predict)-alpha/2*np.sum(self.coef_/np.abs(self.coef_)))).reshape(-1,1)
+    def calculate_loss(self, X, y, coefs, alpha):
+        return 1/X.shape[0]*(np.sum(y-np.matmul(coefs, X.T))**2+alpha*np.sum(np.abs(coefs)))
 
-    def calculate_loss(self, X, y, y_predict, coef):
-        return 1/X.shape[0]*(np.sum(y-y_predict)**2+self.alpha*np.sum(np.abs(coef)))
+    def calculate_derivative_of_loss(self, X, y, coefs, alpha):
+        return -2/X.shape[0]*(np.dot(X.T, y-np.matmul(coefs, X.T))-alpha/2*coefs/np.abs(coefs))
 
     def predict(self, X):
         self.check_fit(fit_used=self.fit_used)
         X = self.check_X(X=X, train=False)
         X = self.check_for_object_columns(X=X)
         X = self.check_intercept(X=X)
-        predictions_array = self.get_predictions(X=X)
-        final_predictions = self.convert_to_original_classes(predictions_array=predictions_array)
-        return final_predictions
+        predictions = self.get_predictions(X=X, coef=self.coef_)
+        print(predictions[:10])
+        predictions = np.argmax(predictions, axis=1)
+        predictions = self.convert_to_original_classes(predictions=predictions)
+        return predictions
     
     def predict_proba(self, X):
         self.check_fit(fit_used=self.fit_used)
         X = self.check_X(X=X, train=False)
         X = self.check_for_object_columns(X=X)
         X = self.check_intercept(X=X)
-        predictions_array = self.get_predictions(X=X)
-        probabilities_array = self.get_proba(predictions_array=predictions_array)
+        predictions = self.get_predictions(X=X, coef=self.coef_)
+        probabilities_array = self.get_proba(predictions=predictions)
         return probabilities_array
     
     def check_fit(self, fit_used):
         if fit_used == False:
             raise AttributeError('Lasso has to be fitted first.')
     
-    def get_predictions(self, X):
-        return np.matmul(X, self.coef_).squeeze()
+    def get_predictions(self, X, coef):
+        return np.matmul(X, coef)
     
-    def get_proba(self, predictions_array):
-        if(len(self.original_categories) == 2):
-            scaler = MinMaxScaler()
-            probabilities_array = scaler.fit_transform(predictions_array.reshape(-1,1))
-            probabilities_array = np.column_stack([1-probabilities_array, probabilities_array])
-        else:
-            scaler = MinMaxScaler()
-            probabilities_array = scaler.fit_transform(predictions_array)
-        return probabilities_array
+    def get_proba(self, predictions):
+        scaler = MinMaxScaler()
+        probabilities_array = scaler.fit_transform(predictions)
+        normalized_probabilities_array = probabilities_array/np.sum(probabilities_array, axis=1)[:,None]
+        return normalized_probabilities_array
     
-    def convert_to_original_classes(self, predictions_array):
-        if(len(self.original_categories) == 2):
-            y_predict = np.where(predictions_array > 0, self.original_categories[1], self.original_categories[0])
-        else:
-            indices_of_ones = np.argmax(predictions_array, axis=1)
-            y_predict = self.original_categories[indices_of_ones]
-        return y_predict
+    def convert_to_original_classes(self, predictions):
+        return np.array([self.original_classes[klasa] for klasa in predictions])
