a83b321c524dac4865519c88d999143a12c570da
Kuba <kuba.koscian001@gmail.com>
2023-08-19 14:41:45 +0200

    Decision Tree Regressor from scratch

--- Decision_Tree/Model/Decision_Tree_Regressor_from_scratch.py
+++ Decision_Tree/Model/Decision_Tree_Regressor_from_scratch.py
@@ -73,49 +73,36 @@ class Decision_Tree_Regressor():
         return np.array(X)
     
     def build_tree(self, X, y, depth):
-        reduction, question, condition, true_rows, false_rows = self.find_best_split(X=X, y=y)
-        # Base case: no further info gain. Since we can ask no further questions, we'll return a leaf.
-        if reduction == 0:
-            return Leaf(y=y)
-        # If we reach here, we have found a useful feature / value to partition on.
-        X_true_subset = X[true_rows,:]
-        X_false_subset = X[false_rows,:]
-        y_true_subset = y[true_rows]
-        y_false_subset = y[false_rows]
-        # Recursively build the true branch.
-        true_branch = self.build_tree(X=X_true_subset, y=y_true_subset, depth=depth+1)
-        # Recursively build the false branch.
-        false_branch = self.build_tree(X=X_false_subset, y=y_false_subset, depth=depth+1)
-        prediction_in_current_node = self.mean_of_target_in_node(y=y)
-        if(depth <= self.max_depth):
-            self.calculate_node_importance(y=y, true_rows=true_rows, false_rows=false_rows, column=condition["column"])
-        return Decision_Node(question, condition, true_branch, false_branch, prediction_in_current_node)
+        best_split = self.find_best_split(X=X, y=y)
+        if(best_split["reduction"] != 0 and depth <= self.max_depth):
+            true_branch = self.build_tree(X=X[best_split["true_rows"], :], y=y[best_split["true_rows"]], depth=depth+1)
+            self.calculate_node_importance(y=y, true_rows=best_split["true_rows"], false_rows=best_split["false_rows"], feature=best_split["feature"])
+            false_branch = self.build_tree(X=X[best_split["false_rows"], :], y=y[best_split["false_rows"]], depth=depth+1)
+            self.calculate_node_importance(y=y, true_rows=best_split["true_rows"], false_rows=best_split["false_rows"], feature=best_split["feature"])
+            return Decision_Node(true_branch=true_branch, false_branch=false_branch, feature=best_split["feature"], interpolation_value=best_split["interpolation_value"])
+        return Leaf(y=y)
 
     def find_best_split(self, X, y):
-        best_reduction = 0
-        best_question = ""
-        best_condition = {}
-        best_true_rows = None
-        best_false_rows = None
-        error_current_subset = self.calculate_error(y=y)
-        condition = {}
+        best_split = {"reduction": 0, "feature": None, "true_rows": None, "false_rows": None, "interpolation_value": None}
+        error_root_node = self.calculate_error(y=y)
         limited_columns = self.get_limited_columns(X=X)
-        for column in limited_columns:
-            interpolation_values = set([np.quantile(np.array(X)[:,column], q=i, method="midpoint") for i in [0, 0.2, 0.4, 0.6, 0.8, 1]])
+        for feature in limited_columns:
+            interpolation_values = self.find_interpolation_values(X=X[:,feature])
             for interpolation_value in interpolation_values:
-                question = f"Is feature[{column}] <= {interpolation_value}"
-                condition["column"] = column
-                condition["interpolation_value"] = interpolation_value
-                true_rows, false_rows = self.partition(X=X, column=column, interpolation_value=interpolation_value)
+                true_rows, false_rows = self.partition(X=X, feature=feature, interpolation_value=interpolation_value)
                 # Skip this split if it doesn't divide the dataset. If it will happen for all the columns then it means that node is a leaf.
                 if len(true_rows) == 0 or len(false_rows) == 0:
                     continue
-                # Calculate the information gain from this split
+                # Calculate the reduction from this split
                 avg_error_child = self.calculate_avg_error_child(y=y, true_rows=true_rows, false_rows=false_rows)
-                reduction = self.calculate_reduction(error_current_subset=error_current_subset, avg_error_child=avg_error_child)
-                if reduction >= best_reduction and len(true_rows)+len(false_rows) > self.min_samples_split:
-                    best_reduction, best_question, best_condition, best_true_rows, best_false_rows = reduction, question, condition.copy(), true_rows, false_rows
-        return best_reduction, best_question, best_condition, best_true_rows, best_false_rows
+                reduction = self.calculate_reduction(error_root_node=error_root_node, avg_error_child=avg_error_child)
+                if reduction >= best_split["reduction"] and len(true_rows)+len(false_rows) > self.min_samples_split:
+                    best_split["reduction"] = reduction
+                    best_split["feature"] = feature
+                    best_split["true_rows"] = true_rows
+                    best_split["false_rows"] = false_rows
+                    best_split["interpolation_value"] = interpolation_value
+        return best_split
     
     def get_limited_columns(self, X):
         upper_bound = {None: random.sample([i for i in range(0, X.shape[1])], int(X.shape[1])),
@@ -131,8 +118,14 @@ class Decision_Tree_Regressor():
             else:
                 raise ValueError("max_features cannot be bigger than number of columns in X.")
     
-    def partition(self, X, column, interpolation_value):
-        return np.where(np.array(X)[:,column] <= interpolation_value)[0], np.where(np.array(X)[:,column] > interpolation_value)[0]
+    def find_interpolation_values(self, X):
+        if(len(np.unique(X)) > 5):
+            return set([np.quantile(X, q=i, method="midpoint") for i in [0, 0.2, 0.4, 0.6, 0.8, 1]])
+        else:
+            return (X[1:] + X[:-1]) / 2
+    
+    def partition(self, X, feature, interpolation_value):
+        return np.where(np.array(X)[:,feature] <= interpolation_value)[0], np.where(np.array(X)[:,feature] > interpolation_value)[0]
     
     def calculate_error(self, y):
         error = {"squared_error": np.mean((y - np.mean(y)) ** 2),
@@ -144,19 +137,19 @@ class Decision_Tree_Regressor():
         error_subset_2 = self.calculate_error(y=y[false_rows])
         return len(true_rows)/(len(true_rows)+len(false_rows))*error_subset_1+len(false_rows)/(len(true_rows)+len(false_rows))*error_subset_2
 
-    def calculate_reduction(self, error_current_subset, avg_error_child):
-        return error_current_subset - avg_error_child
+    def calculate_reduction(self, error_root_node, avg_error_child):
+        return error_root_node - avg_error_child
     
-    def calculate_node_importance(self, y, true_rows, false_rows, column):
+    def calculate_node_importance(self, y, true_rows, false_rows, feature):
         n_node = len(y[true_rows])+len(y[false_rows])
         N = self.X_train.shape[0]
-        uncertainty_node = self.calculate_error(y=y)
+        error_root_node = self.calculate_error(y=y)
         n_subset_left = len(y[true_rows])
         n_subset_right = len(y[false_rows])
-        uncertainty_subset_1 = self.calculate_error(y=y[true_rows])
-        uncertainty_subset_2 = self.calculate_error(y=y[false_rows])
-        node_importance = n_node/N*(uncertainty_node-n_subset_left/n_node*uncertainty_subset_1-n_subset_right/n_node*uncertainty_subset_2)
-        self.feature_importances_[f"Feature_{column}"] += node_importance
+        error_subset_1 = self.calculate_error(y=y[true_rows])
+        error_subset_2 = self.calculate_error(y=y[false_rows])
+        node_importance = n_node/N*(error_root_node-n_subset_left/n_node*error_subset_1-n_subset_right/n_node*error_subset_2)
+        self.feature_importances_[f"Feature_{feature}"] += node_importance
     
     def predict(self, X):
         self.check_fit(fit_used=self.fit_used)
@@ -164,51 +157,29 @@ class Decision_Tree_Regressor():
         X = self.check_for_object_columns(X=X)
         predictions = []
         for row in range(0, X.shape[0]):
-            predictions.append(self.get_score(row=X[row,:], node=self.tree))
+            predictions.append(self.get_score(row=X[row,:], node=self.tree, depth=0))
         return np.array(predictions)
     
     def get_score(self, row, node, depth=0):
         if isinstance(node, Leaf):
             return node.prediction
-        elif(depth <= self.max_depth):
-            if(row[node.condition["column"]] <= node.condition["interpolation_value"]):
+        else:
+            if(row[node.feature] <= node.interpolation_value):
                 return self.get_score(row=row, node=node.true_branch, depth=depth+1)
             else:
                 return self.get_score(row=row, node=node.false_branch, depth=depth+1)
-        else:
-            return node.prediction_in_current_node
-    
-    def mean_of_target_in_node(self, y):
-        return np.mean(y)
     
     def check_fit(self, fit_used):
         if fit_used == False:
             raise AttributeError('Decision_Tree_Regressor has to be fitted first.')
-    
-    def print_tree(self, node=None, spacing=""):
-        if(node == None):
-            node = self.tree
-        if isinstance(node, Leaf):
-            print (spacing + "Predict", node.prediction)
-            return
-        print (spacing + str(node.question))
-
-        # Call this function recursively on the true branch
-        print (spacing + '--> True:')
-        self.print_tree(node.true_branch, spacing + "  ")
-
-        # Call this function recursively on the false branch
-        print (spacing + '--> False:')
-        self.print_tree(node.false_branch, spacing + "  ")
 
 class Leaf:
     def __init__(self, y):
-        self.prediction = Decision_Tree_Regressor().mean_of_target_in_node(y=y)
+        self.prediction = np.mean(y)
 
 class Decision_Node:
-    def __init__(self, question, condition, true_branch, false_branch, prediction_in_current_node):
-        self.question = question
-        self.condition = condition
+    def __init__(self, true_branch, false_branch, feature, interpolation_value):
         self.true_branch = true_branch
         self.false_branch = false_branch
-        self.prediction_in_current_node = prediction_in_current_node
+        self.feature = feature
+        self.interpolation_value = interpolation_value
